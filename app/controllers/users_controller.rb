b083e99983925e7ba493988ed8ef308a88aae195
Sam Uherek <samuherekbiz@gmail.com>
2015-11-28 20:46:26 +0800

    fuck yeah... added user profile page and linking from index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,15 @@
 class UsersController < ApplicationController
 
-  def show
+  def index
+    @users = User.all
   end
+
+  def show
+    @user = User.find(params[:id])
+    @posts = User.find(params[:id]).posts.order("created_at DESC")
+  end
+
+  private
+
 
 end
